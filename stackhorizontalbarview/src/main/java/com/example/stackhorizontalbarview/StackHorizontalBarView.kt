ba43940193a01624a468b09415fa4806851c1375
Anwesh43 <anweshthecool0@gmail.com>
2021-03-07 01:12:12 +0530

    adding StackHorizontalBarView class

--- stackhorizontalbarview/src/main/java/com/example/stackhorizontalbarview/StackHorizontalBarView.kt
+++ stackhorizontalbarview/src/main/java/com/example/stackhorizontalbarview/StackHorizontalBarView.kt
@@ -80,3 +80,19 @@ fun Canvas.drawSHBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawStackHorizontalBars(i, scale, w, h, paint)
 }
+
+class StackHorizontalBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
