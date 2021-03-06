e615a5dc77b492acd0dbebf8aec3b16471dadf21
Anwesh43 <anweshthecool0@gmail.com>
2021-03-07 01:01:59 +0530

    adding Int and Float extension methods

--- stackhorizontalbarview/src/main/java/com/example/stackhorizontalbarview/StackHorizontalBarView.kt
+++ stackhorizontalbarview/src/main/java/com/example/stackhorizontalbarview/StackHorizontalBarView.kt
@@ -23,3 +23,11 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val bars : Int = 5
 val scGap : Float = 0.02f / bars
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.aboveScale(i : Int, n : Int) : Float = ((i + 1)..(n-1)).map {
+    divideScale(it, n)
+}.reduce { a : Float, b : Float -> a + b }
